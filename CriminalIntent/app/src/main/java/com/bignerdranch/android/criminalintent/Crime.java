02109deb4a71c9ebf6deecc5625086d242baf13f
David Cordero <dcorderoramirez@gmail.com>
2015-09-01 22:15:38 +0200

    Add new attributes to Crime model

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,14 +1,18 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     // Accesors
@@ -23,5 +27,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
     }
 }
