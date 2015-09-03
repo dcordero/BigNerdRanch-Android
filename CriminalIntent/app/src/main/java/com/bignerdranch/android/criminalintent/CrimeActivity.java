da0289213e2c07fb421705bc9f3abf28f16a269e
David Cordero <david.cordero@schibsted.com>
2015-09-03 07:43:26 +0200

    Create generic single fragment activity

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -5,19 +5,10 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
