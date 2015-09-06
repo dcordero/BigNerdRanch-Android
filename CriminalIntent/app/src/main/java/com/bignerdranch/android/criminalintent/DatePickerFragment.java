78fa85b752de59cb48263ff1eec8044817d3a988
David Cordero <dcorderoramirez@gmail.com>
2015-09-06 12:33:15 +0200

    Add date picker in the dialog

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class DatePickerFragment extends DialogFragment {
 
@@ -12,7 +14,10 @@ public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        View datePickerView = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(datePickerView)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
