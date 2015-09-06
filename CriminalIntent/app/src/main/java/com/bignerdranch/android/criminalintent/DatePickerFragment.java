84081c2d507a181c5cd71661d541651fd44ba1b6
David Cordero <dcorderoramirez@gmail.com>
2015-09-06 12:18:18 +0200

    Create DialogFragment

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v7.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
