93f732cf6712172989f9aec022ba0995d7da72f0
David Cordero <dcorderoramirez@gmail.com>
2015-09-12 16:27:28 +0200

    Create table

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class CrimeBaseHelper extends SQLiteOpenHelper
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "crimeBase.db";
@@ -15,7 +17,13 @@ public class CrimeBaseHelper extends SQLiteOpenHelper
     }
 
     @Override public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + CrimeTable.NAME
+                        + "( _id integer primary key autoincrement, "
+                        + CrimeTable.Cols.UUID + ", "
+                        + CrimeTable.Cols.TITLE + ", "
+                        + CrimeTable.Cols.DATE + ", "
+                        + CrimeTable.Cols.SOLVED + ")"
+                    );
     }
 
     @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
