3566dcf4da264424c57e3f0a8c317276076e0202
David Cordero <dcorderoramirez@gmail.com>
2015-09-28 00:01:22 +0200

    Create alias to load one layout or the other depending on the screen width

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -8,4 +8,9 @@ public class CrimeListActivity extends SingleFragmentActivity {
     protected Fragment createFragment() {
         return new CrimeListFragment();
     }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
+    }
 }
