8df34aa6024c414358e17b4975412373f732b053
David Cordero <dcorderoramirez@gmail.com>
2015-09-28 00:32:02 +0200

    CrimeListActivity implements the callbacks interfacte of CrimeListFragment

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,7 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
 
     @Override
     protected Fragment createFragment() {
@@ -12,5 +12,10 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        
     }
 }
