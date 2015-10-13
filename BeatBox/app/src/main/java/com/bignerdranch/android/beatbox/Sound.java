9073b9ebb28cceda1c694d980b6ba55ef7e9a73a
David Cordero <dcorderoramirez@gmail.com>
2015-10-14 00:58:11 +0200

    Create class Sound to manage each wav asset

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -1,7 +1,23 @@
 package com.bignerdranch.android.beatbox;
 
-/**
- * Created by dcordero on 14/10/15.
- */
 public class Sound {
+    private String mAssetPath;
+
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+
+        String[] components = assetPath.split("/");
+        String fileName = components[components.length - 1];
+        mName = fileName.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
 }
