ab04f0960e2641f8ac007f09e6ada95da1e68852
David Cordero <dcorderoramirez@gmail.com>
2015-10-14 01:01:14 +0200

    Modify BeatBox to build Sound objects

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -5,12 +5,15 @@ import android.content.res.AssetManager;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BeatBox {
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
 
     private AssetManager mAssetsManager;
+    private List<Sound> mSounds = new ArrayList<>();
 
     public BeatBox(Context context) {
         mAssetsManager = context.getAssets();
@@ -25,6 +28,17 @@ public class BeatBox {
         }
         catch (IOException ioe) {
             Log.e(TAG, "Couldn't list assets", ioe);
+            return;
         }
+
+        for (String soundName : soundNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + soundName;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
     }
 }
