005479179c1b734f1fd3faebfa5a8caaee9a7662
David Cordero <dcorderoramirez@gmail.com>
2015-10-17 00:15:40 +0200

    Retain fragment to avoid interruptions in the sound on rotations

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -22,6 +22,7 @@ public class BeatBoxFragment extends Fragment {
 
     @Override public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
         mBeatBox = new BeatBox(getActivity());
     }
 
