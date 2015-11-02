6b3f2be6dbc3d6500505046a5573aab83e81f3bc
David Cordero <dcorderoramirez@gmail.com>
2015-11-02 20:52:19 +0100

    Add RecyclerView

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,9 +1,16 @@
 package com.bignerdranch.android.photogallery;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment newInstance() {
 
@@ -12,5 +19,13 @@ public class PhotoGalleryFragment extends Fragment {
         PhotoGalleryFragment fragment = new PhotoGalleryFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+
+        return view;
     }
 }
