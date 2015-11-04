e44978eaba2f4f40c668b08fb6b187ac4fe16859
David Cordero <dcorderoramirez@gmail.com>
2015-11-04 21:38:50 +0100

    Get json result from Flickr API

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,6 +10,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FlickrFetcher {
+
+    private static final String TAG = "FlickrFetcher";
+
+    private static final String API_KEY = "SECRET_KEY_HERE";
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -35,5 +42,25 @@ public class FlickrFetcher {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+
+            String jsonString = null;
+
+            jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (IOException e) {
+            Log.i(TAG, "Failed to fetch: " + e);
+        }
     }
 }
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -6,12 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 public class PhotoGalleryFragment extends Fragment {
 
@@ -21,12 +18,7 @@ public class PhotoGalleryFragment extends Fragment {
 
     private class FetchItemsTasks extends AsyncTask<Void, Void, Void> {
         @Override protected Void doInBackground(Void... params) {
-            try {
-                String result = new FlickrFetcher().getUrlString("https://www.bignerdranch.com");
-                Log.e(TAG, "Fetched contents from URL: " + result);
-            } catch (IOException e) {
-                Log.e(TAG, "Failed to fetch URL: " + e);
-            }
+            new FlickrFetcher().fetchItems();
             return null;
         }
     }
