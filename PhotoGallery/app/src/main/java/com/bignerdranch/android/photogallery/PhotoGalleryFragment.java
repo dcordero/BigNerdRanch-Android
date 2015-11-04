250f8a8c58e9b5df3301e6bb22b1299d80026637
David Cordero <dcorderoramirez@gmail.com>
2015-11-04 21:57:21 +0100

    Create ViewHolder for Gallery items

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PhotoGalleryFragment extends Fragment {
 
@@ -45,5 +46,19 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
     }
 }
