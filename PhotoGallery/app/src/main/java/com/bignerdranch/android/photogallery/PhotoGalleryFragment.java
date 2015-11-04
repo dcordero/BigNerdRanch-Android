9b2a3e65e58dbd4f37518ef6d06b43003d8520a6
David Cordero <dcorderoramirez@gmail.com>
2015-11-04 01:17:47 +0100

    Fetch url using asyntask I hope just as a temporal solution to explain how wrong it is

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,17 +1,35 @@
 package com.bignerdranch.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
+
+    private class FetchItemsTasks extends AsyncTask<Void, Void, Void> {
+        @Override protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetcher().getUrlString("https://www.bignerdranch.com");
+                Log.e(TAG, "Fetched contents from URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: " + e);
+            }
+            return null;
+        }
+    }
 
     public static PhotoGalleryFragment newInstance() {
 
@@ -25,6 +43,7 @@ public class PhotoGalleryFragment extends Fragment {
     @Override public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTasks().execute();
     }
 
     @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
